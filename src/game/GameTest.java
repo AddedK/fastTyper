5b07c8942bc82a1415da3d2c7abc6c5b048d7eca
Added Kina <addedk@gmail.com>
2022-06-19 10:37:11 +0200

    add predictionarray test. Add game constructor test

--- src/game/GameTest.java
+++ src/game/GameTest.java
@@ -6,14 +6,39 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
 
+
+    /**
+     * CreatePredictionArray tests
+     */
     @Test
-    public void testCreatePredictionArray() {
+    public void testCreatePredictionArray1() {
         String string1 = "I like to party.";
         String[] answer1 = {"I ", "like ", "to ", "party."};
         String[] prediction1 = Game.createPredictionArray(string1);
         assertArrayEquals(answer1,prediction1);
 
+    }
 
+    @Test
+    public void testCreatePredictionArray2() {
+        String string1 = "I like to party.\nThis is great.\nSuper fun.";
+        String[] answer1 = {"I ", "like ", "to ", "party. ", "This ","is ","great. ","Super ","fun."};
+        String[] prediction1 = Game.createPredictionArray(string1);
+        assertArrayEquals(answer1,prediction1);
+    }
+
+    @Test
+    public void testGame() {
+        String predictionString1 = "I like to party.";
+        String[] answer1 = {"I ", "like ", "to ", "party."};
+
+        Game game = new Game(predictionString1,false);
+
+        String[] gamePredictionArray = game.getPredictionArray();
+        assertArrayEquals(answer1,gamePredictionArray);
+
+        String gameCurrentTypedWord = game.getCurrentTypedWord();
+        assertEquals(gameCurrentTypedWord,"");
 
     }
 
