858f8999f742a195d4450a148e1b2ac42da90a42
Added Kina <addedk@gmail.com>
2022-07-01 18:40:06 +0200

    text reader uses private variables to decide what is the current files and if there are more files to be read

--- src/game/PredictionTextReader.java
+++ src/game/PredictionTextReader.java
@@ -10,16 +10,18 @@ public class PredictionTextReader {
     private String directoryOfPredictionFiles; // The directory where it looks for text files
     private String[] listOfPredictionFileNames;
     private int currentPredictionFileIndex;
+    private boolean hasMoreFiles;
 
     public PredictionTextReader(String directoryOfPredictionFiles) {
         this.directoryOfPredictionFiles = directoryOfPredictionFiles;
         this.currentPredictionFileIndex = 0;
         this.listOfPredictionFileNames = getFilesFromDirectory();
+        this.hasMoreFiles = true;
 
     }
 
-    public String textFileToPrediction(int fileIndex) {
-        String fileName = this.listOfPredictionFileNames[fileIndex];
+    public String textFileToPrediction() {
+        String fileName = this.listOfPredictionFileNames[this.currentPredictionFileIndex];
         StringBuilder sb = new StringBuilder();
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             String line;
@@ -35,6 +37,15 @@ public class PredictionTextReader {
         catch (Exception e) {
             return "";
         }
+    }
+
+    public void incrementPredictionFileIndex() {
+        if (this.currentPredictionFileIndex < this.listOfPredictionFileNames.length-1) {
+            this.currentPredictionFileIndex++;
+        } else {
+            this.hasMoreFiles = false;
+        }
+
     }
 
 
@@ -53,7 +64,17 @@ public class PredictionTextReader {
     /**
      * Getter and setters
      */
-    // TODO
+    public int getCurrentPredictionFileIndex() {
+        return currentPredictionFileIndex;
+    }
+
+    public void setCurrentPredictionFileIndex(int currentPredictionFileIndex) {
+        this.currentPredictionFileIndex = currentPredictionFileIndex;
+    }
+
+    public boolean hasMoreFiles(){
+        return this.hasMoreFiles;
+    }
 
     public static void main(String[] args) throws FileNotFoundException {
         PredictionTextReader ptr = new PredictionTextReader("src/game/predictionFiles1");
@@ -61,8 +82,8 @@ public class PredictionTextReader {
         for (int i = 0; i < ptr.listOfPredictionFileNames.length ; i++) {
             System.out.println(ptr.listOfPredictionFileNames[i]);
         }
-        System.out.println(ptr.textFileToPrediction(0));
-        System.out.println(ptr.textFileToPrediction(1));
+        System.out.println(ptr.textFileToPrediction());
+        System.out.println(ptr.textFileToPrediction());
 
     }
 }
