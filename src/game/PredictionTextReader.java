5fafca056fabe84af8d3fdf6364fc1c99c82b83f
Added Kina <addedk@gmail.com>
2022-07-01 09:46:18 +0200

    create empty class meant for reading textFiles and generating prediction strings for game

---
+++ src/game/PredictionTextReader.java
@@ -0,0 +1,20 @@
+/**
+ * The purpose of this class is to read textFiles and generate prediction strings to the Game class
+ */
+
+package game;
+
+public class PredictionTextReader {
+    private String directoryOfPredictionFiles;
+    private String[] listOfPredictionFiles;
+    private int currentPredictionFileIndex;
+
+
+    public PredictionTextReader() {
+    }
+
+    public String textFileToPrediction(String pathToTextFile){
+
+        return "TODO";
+    }
+}
